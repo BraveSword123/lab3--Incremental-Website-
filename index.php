91fbaeda36ae9781868332443bb003f013937d21
BraveSword123 <annabelle.gyeniba.moore@gmail.com>
2026-08-17 12:17:46 -0700

    Formatted the HTML Code, so it looks neat

--- index.php
+++ index.php
@@ -1,138 +1,137 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab 3</title> 
-     <style>
-        body{
-            padding-right: 100px;
-            font-family: 'Times New Roman', Times, serif;
-            font-size: 25px;
-        }
-
-        #wrapper{
-            display: flex; 
-            flex-direction: column;
-            align-items: end;             
-            justify-content: center; 
-            position: relative;
-            top: 100px; 
-        }
-
-        h1{
-            color: #407DFF; 
-            font-family: 'Brush Script MT', 'Times New Roman', Times, serif;
-            font-size: 90px;
-            display: inline; 
-        }
-
-        h2{
-            font-size: 20px; 
-            font-style: italic;
-            color: #7BA5FB; 
-            text-align: center;
-            margin-top: -10px; 
-        }
-
-        main{
-            background-color: #CFDEFF;
-            padding: 60px; 
-            border-radius: 5px;
-        }
-
-        input[type="submit"] {
-            background-color: #CFDEFF;
-            border-radius: 5px; 
-            border: 0px;  
-            font-size: 22px;
-            padding: 18px; 
-            font-family: 'Times New Roman', Times, serif;
-            cursor: pointer;
-        }
-
-        input[type="submit"]{
-            background-color: #93B6FF;
-        }
-
-        input[type="submit"]:hover{
-             transition: 0.5s;
-             background-color: #76a1ff;
-        }
-
-        input[type="text"], input[type="password"]{
-            width: 210px; 
-            height: 20px;
-        }
-
-        input[type="submit"]:active{
-            transition: 0.5s;
-            transform: scale(0.98);
-        }
-
-        div{
-            display: flex; 
-            flex-direction: row;
-            column-gap: 75px; 
-        }
-
-        img{
-            height: 1000px; 
-            position: absolute; 
-            left: -10px; 
-            top: 360px; 
-            transform: translateY(-50%);
-            display: block; 
-        }
-
-        label{
-            font-size: 22px; 
-        }
-
-        div header, div main {
-            position: relative; 
-        }  
-
-        div main{
-            left: -10%; 
-        }
-
-        div header{
-            left: -13%; 
-        }
-
-        @media screen and (max-width: 1185px){
-            img{
-                display: none; 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lab 3</title> 
+        <style>
+            body{
+                padding-right: 100px;
+                font-family: 'Times New Roman', Times, serif;
+                font-size: 25px;
             }
-        }
 
-     </style> 
-</head>
-<body>
- <div id="wrapper">
-    <header>
-        <h1>Incremental</h1>
-        <h2>The Counting Website</h2>
-    </header>
-    <br>
-    <main>
-        <form action="lab3.php" method="get">
-            <label for="username">Username:</label> 
+            #wrapper{
+                display: flex; 
+                flex-direction: column;
+                align-items: end;             
+                justify-content: center; 
+                position: relative;
+                top: 100px; 
+            }
+
+            h1{
+                color: #407DFF; 
+                font-family: 'Brush Script MT', 'Times New Roman', Times, serif;
+                font-size: 90px;
+                display: inline; 
+            }
+
+            h2{
+                font-size: 20px; 
+                font-style: italic;
+                color: #7BA5FB; 
+                text-align: center;
+                margin-top: -10px; 
+            }
+
+            main{
+                background-color: #CFDEFF;
+                padding: 60px; 
+                border-radius: 5px;
+            }
+
+            input[type="submit"] {
+                background-color: #CFDEFF;
+                border-radius: 5px; 
+                border: 0px;  
+                font-size: 22px;
+                padding: 18px; 
+                font-family: 'Times New Roman', Times, serif;
+                cursor: pointer;
+            }
+
+            input[type="submit"]{
+                background-color: #93B6FF;
+            }
+
+            input[type="submit"]:hover{
+                transition: 0.5s;
+                background-color: #76a1ff;
+            }
+
+            input[type="text"], input[type="password"]{
+                width: 210px; 
+                height: 20px;
+            }
+
+            input[type="submit"]:active{
+                transition: 0.5s;
+                transform: scale(0.98);
+            }
+
+            div{
+                display: flex; 
+                flex-direction: row;
+                column-gap: 75px; 
+            }
+
+            img{
+                height: 1000px; 
+                position: absolute; 
+                left: -10px; 
+                top: 360px; 
+                transform: translateY(-50%);
+                display: block; 
+            }
+
+            label{
+                font-size: 22px; 
+            }
+
+            div header, div main {
+                position: relative; 
+            }  
+
+            div main{
+                left: -10%; 
+            }
+
+            div header{
+                left: -13%; 
+            }
+
+            @media screen and (max-width: 1185px){
+                img{
+                    display: none; 
+                }
+            }
+        </style> 
+    </head>
+    <body>
+        <div id="wrapper">
+            <header>
+                <h1>Incremental</h1>
+                <h2>The Counting Website</h2>
+            </header>
             <br>
-            <input type="text" id="username" name="username"> 
-            <br>
-            <br>
-            <label for="password">Password:</label> 
-            <br> 
-            <input type="password" id="password" name="password"> 
-            <br> 
-            <br> 
-            <input type="submit" value="Login" name="login">
-            <input type="submit" value="Create Account" name="createAccount">
-        </form>
-    </main>
-    <img src="Sign In Image.png"> 
-</div>
-</body>
+            <main>
+                <form action="lab3.php" method="get">
+                    <label for="username">Username:</label> 
+                    <br>
+                    <input type="text" id="username" name="username"> 
+                    <br>
+                    <br>
+                    <label for="password">Password:</label> 
+                    <br> 
+                    <input type="password" id="password" name="password"> 
+                    <br> 
+                    <br> 
+                    <input type="submit" value="Login" name="login">
+                    <input type="submit" value="Create Account" name="createAccount">
+                </form>
+            </main>
+            <img src="Sign In Image.png"> 
+        </div>
+    </body>
 </html>
--- lab3.php
+++ lab3.php
@@ -1,10 +1,10 @@
 <?php
-//  This is the start of PHP code 
+// This is the start of PHP code 
 // Connect to mySQL database
-$connection = new mysqli("localhost", "root", "root", "incremental_account");
+    $connection = new mysqli("localhost", "root", "root", "incremental_account");
 
 // If we were not able to connect to the database do this 
-if($connection->connect_error){
+    if($connection->connect_error){
 
     // The period does string concatenation (like + in Javascript)
     print("Error is: " . $connection->connect_error); 
@@ -14,197 +14,191 @@ if($connection->connect_error){
 }
 
 // Start the session 
-session_start(); 
+    session_start(); 
 
 // If the username doesnt exist make this equal to null 
-$user = $_REQUEST["username"] ?? null; 
-$pass = $_REQUEST["password"] ?? null; 
+    $user = $_REQUEST["username"] ?? null; 
+    $pass = $_REQUEST["password"] ?? null; 
 
 // Check if the username has already been saved 
- if(isset($_SESSION["user"])){
+    if(isset($_SESSION["user"])){
  // If it has save that value in a variable 
-    $session_user = $_SESSION["user"]; 
-}
+        $session_user = $_SESSION["user"]; 
+    }
 // If the username hasn't been saved 
-else{
+    else{
     // Save the usewrname in theb session  
-    $_SESSION["user"] = $user;
+        $_SESSION["user"] = $user;
     // Save the username in a variable 
-    $session_user = $_SESSION["user"];  
-}  
+        $session_user = $_SESSION["user"];  
+    }  
 
     print("Welcome Back, ". $_SESSION["user"]); 
 ?> 
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab 3</title>
-    <style>
-        body{
-            background-image: url("Lab 3 Image.png");
-            background-size: cover;
-            background-position: center;
-            background-position-y: center; 
-            background-attachment: fixed;
-            background-repeat: no-repeat;
-            font-family: 'Times New Roman', Times, serif;
-            display: flex; 
-            flex-direction: column;
-            align-items: center; 
-            justify-content: center;
-            border: 15px solid #93B6FF; 
-            margin: 0; 
-            height: 835px; 
-            color: #407DFF; 
-            overflow: hidden; 
-        }
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lab 3</title>
+        <style>
+            body{
+                background-image: url("Lab 3 Image.png");
+                background-size: cover;
+                background-position: center;
+                background-position-y: center; 
+                background-attachment: fixed;
+                background-repeat: no-repeat;
+                font-family: 'Times New Roman', Times, serif;
+                display: flex; 
+                flex-direction: column;
+                align-items: center; 
+                justify-content: center;
+                border: 15px solid #93B6FF; 
+                margin: 0; 
+                height: 835px; 
+                color: #407DFF; 
+                overflow: hidden; 
+            }
 
-        span{
-            font-size: 200px; 
-            margin-bottom: 0; 
-        }
+            span{
+                font-size: 200px; 
+                margin-bottom: 0; 
+            }
 
-        input[type="submit"]{
-            margin-top: -10px; 
-            height: 70px; 
-            width: 200px; 
-            cursor: pointer;
-            font-size: 24px;
-            font-family: 'Times New Roman', Times, serif;
-            background-color: #CFDEFF;
-            border: none; 
-            border-radius: 10px;
-        }
+            input[type="submit"]{
+                margin-top: -10px; 
+                height: 70px; 
+                width: 200px; 
+                cursor: pointer;
+                font-size: 24px;
+                font-family: 'Times New Roman', Times, serif;
+                background-color: #CFDEFF;
+                border: none; 
+                border-radius: 10px;
+            }
 
-        input[type="submit"]:hover{
-            transition: 0.5s;
-            background-color: #b1caff;
-        }
+            input[type="submit"]:hover{
+                transition: 0.5s;
+                background-color: #b1caff;
+            }
 
-       input[type="submit"]:active{
-            transition: 0.5s;
-            transform: scale(0.98);
-        }
+            input[type="submit"]:active{
+                transition: 0.5s;
+                transform: scale(0.98);
+            }
 
-        #signOutButton{
-            align-self: flex-end;
-            position: fixed; 
-            right: 20px; 
-            bottom: 20px; 
-        }
+            #signOutButton{
+                align-self: flex-end;
+                position: fixed; 
+                right: 20px; 
+                bottom: 20px; 
+            }
 
-        footer{
-            font-size: 50px;
-            font-family: 'Brush Script MT', 'Times New Roman', Times, serif;
-            position: fixed; 
-            left: 20px; 
-            bottom: -40px; 
-        }
-    </style>
-</head>
-<body>
-    <?php
-    if(isset($_GET["createAccount"])){
+            footer{
+                font-size: 50px;
+                font-family: 'Brush Script MT', 'Times New Roman', Times, serif;
+                position: fixed; 
+                left: 20px; 
+                bottom: -40px; 
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        if(isset($_GET["createAccount"])){
         // This holds an SQL query that reads select everything from the table where the username equals the ?
-        $stmt = $connection->prepare("SELECT * FROM user where username = ?"); 
+            $stmt = $connection->prepare("SELECT * FROM user where username = ?"); 
         // ? is equal to whatever the user input for username 
-        $stmt->bind_param("s", $session_user); 
+            $stmt->bind_param("s", $session_user); 
         // Run the SQL Query 
-        $stmt->execute(); 
+            $stmt->execute(); 
         // The result of the SQL query is help in the userinfo_results variable
-        $userinfo_result = $stmt->get_result(); 
+            $userinfo_result = $stmt->get_result(); 
         // If there is at least one user that already has this username
-        if($userinfo_result->num_rows > 0){
+            if($userinfo_result->num_rows > 0){
             // Here we should send you back to the previous page
             // with a message reading that this username is already 
             // in use 
-            header("Location: index.php"); 
-            // print("Someone already has this username"); 
-            exit; 
-        }
-        else {
-            // print "Userame was not found!";     
+                header("Location: index.php"); 
+                exit; 
+            }
+            else {   
             // Add values to the table that are the question marks
-            $stmt = $connection->prepare("INSERT INTO user (username, password)". "VALUES (?, ?)"); 
+                $stmt = $connection->prepare("INSERT INTO user (username, password)". "VALUES (?, ?)"); 
             // The question marks are $user and $pass 
-            $stmt->bind_param("ss", $session_user, $pass); 
-            // Run the query and give the result to this variable 
-            // the result will be true or false meaning 
-            // was the query executed effectively 
-            $resultIns = $stmt->execute(); 
-        } 
-    }
-    // Need to also check for password too 
-    if(isset($_GET["login"])) {
+                $stmt->bind_param("ss", $session_user, $pass); 
+            // Run the query
+                $resultIns = $stmt->execute(); 
+            } 
+        }
+        // Need to also check for password too 
+            if(isset($_GET["login"])) {
         // This holds an SQL query that reads select everything from the table where the username equals the ?
-        $stmt = $connection->prepare("SELECT * FROM user where username = ? AND password = ?"); 
+            $stmt = $connection->prepare("SELECT * FROM user where username = ? AND password = ?"); 
         // ? is equal to whatever the user input for username 
-        $stmt->bind_param("ss", $session_user, $pass); 
+            $stmt->bind_param("ss", $session_user, $pass); 
         // Run the SQL Query 
-        $stmt->execute(); 
+            $stmt->execute(); 
         // The result of the SQL query is help in the userinfo_results variable
-        $userinfo_result = $stmt->get_result(); 
+            $userinfo_result = $stmt->get_result(); 
         // If there is at least one user that already has this username
-        if($userinfo_result->num_rows > 0){
+            if($userinfo_result->num_rows > 0){
             // Here we should send you back to the previous page
             // with a message reading that this username is already 
             // in use  
-        }
-        else {
-        header("Location: index.php"); 
-    }
-    }
+                }
+                else {
+                    header("Location: index.php"); 
+                }
+            }
 
-    $increment_stmt = $connection->prepare("UPDATE user SET count = count + 1 WHERE username = ?"); 
-    $increment_stmt->bind_param("s", $session_user); 
+            $increment_stmt = $connection->prepare("UPDATE user SET count = count + 1 WHERE username = ?"); 
+            $increment_stmt->bind_param("s", $session_user); 
 
-    // This says if increment=# exist in the URL which isnt what I want
-    // I onlt want this to happen when you click the button 
-    if(isset($_GET["increment"])){
+        // This says if increment=# exist in the URL which isnt what I want
+        // I onlt want this to happen when you click the button 
+            if(isset($_GET["increment"])){
+                $increment_stmt->execute(); 
+            // Server is an array, so we need to use square brackets 
+                header("Location: ". $_SERVER["PHP_SELF"]); 
+            } 
 
-        $increment_stmt->execute(); 
+            $num_stmt = $connection->prepare("SELECT count FROM user WHERE username = ?"); 
+            $num_stmt->bind_param("s", $session_user); 
+            $num_stmt->execute(); 
+            $num_result = $num_stmt->get_result(); 
+            $connection->close(); 
 
-        // Server is an array, so we need to use square brackets 
-        header("Location: ". $_SERVER["PHP_SELF"]); 
-    } 
-
-    $num_stmt = $connection->prepare("SELECT count FROM user WHERE username = ?"); 
-    $num_stmt->bind_param("s", $session_user); 
-    $num_stmt->execute(); 
-    $num_result = $num_stmt->get_result(); 
-    $connection->close(); 
-
-    if(isset($_GET["signOut"])){
-        session_destroy();
-        header("Location: index.php");  
-    }
-?> 
-
-    <main>
-        <?php
-        $user = mysqli_fetch_assoc($num_result); 
-        echo "<span>". $user["count"]. "</span>"; 
+            if(isset($_GET["signOut"])){
+                session_destroy();
+                header("Location: index.php");  
+            }
         ?> 
+
+        <main>
+            <?php
+                $user = mysqli_fetch_assoc($num_result); 
+                echo "<span>". $user["count"]. "</span>"; 
+            ?> 
+            <br>
+            <br>
+        </main>
+        <form action="#" method="get"> 
+            <input id="increment" name="increment" value="+ Increment" type="submit"> 
+        </form>
         <br>
-        <br>
-    </main>
-    <form action="#" method="get"> 
-        <input id="increment" name="increment" value="+ Increment" type="submit"> 
-    </form>
-    <br>
-    <form>
-    <a href="index.php">
-    <input type="submit" value="Sign Out" id="signOutButton" name="signOut"> 
-    </a>
-    </form>
-   <footer>
-            <p>
-                Incremental
-            </p>
-    </footer> 
-</body>
+        <form>
+            <a href="index.php">
+            <input type="submit" value="Sign Out" id="signOutButton" name="signOut"> 
+            </a>
+        </form>
+        <footer>
+                <p>
+                    Incremental
+                </p>
+        </footer> 
+    </body>
 </html>
 
